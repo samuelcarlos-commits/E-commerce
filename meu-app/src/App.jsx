49838edd69a8cfa87daf74c7b3f22d2d91ee5ecf
samuelcarlos-commits <samuel.carlos@escolar.ifrn.edu.br>
2026-09-10 17:06:39 -0300

    new file:   meu-app/.gitignore 	new file:   meu-app/README.md 	new file:   meu-app/eslint.config.js 	new file:   meu-app/index.html 	new file:   meu-app/package-lock.json 	new file:   meu-app/package.json 	new file:   meu-app/public/favicon.svg 	new file:   meu-app/public/icons.svg 	new file:   meu-app/src/App.css 	new file:   meu-app/src/App.jsx 	new file:   meu-app/src/Navbar.css 	new file:   meu-app/src/Navbar.jsx 	new file:   meu-app/src/assets/hero.png 	new file:   meu-app/src/assets/react.svg 	new file:   meu-app/src/assets/vite.svg 	new file:   meu-app/src/index.css 	new file:   meu-app/src/main.jsx 	new file:   meu-app/vite.config.js

---
+++ meu-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Navbar from './Navbar'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navbar></Navbar>
+    </>
+  )
+}
+
+export default App
---
+++ meu-app/src/Navbar.jsx
@@ -0,0 +1,11 @@
+import { React } from "react"
+import './Navbar.css'
+import { GiWitchFace } from "react-icons/gi"
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <h1><GiWitchFace/> Bruxaria</h1>
+        </nav>
+    )
+}

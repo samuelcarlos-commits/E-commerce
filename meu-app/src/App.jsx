106cf4bd1f663f9166df019c01ee1d60860b2e7d
samuelcarlos-commits <samuel.carlos@escolar.ifrn.edu.br>
2026-09-11 16:29:06 -0300

    modified:   meu-app/src/App.jsx 	modified:   meu-app/src/Produto.jsx 	new file:   meu-app/src/style.css

--- meu-app/src/App.jsx
+++ meu-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Navbar from './Navbar'
+import Produto from './Produto'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Produto></Produto>
     </>
   )
 }
--- meu-app/src/Produto.jsx
+++ meu-app/src/Produto.jsx
@@ -1,3 +1,21 @@
+import "./style.css"
 import { useState } from 'react'
 import { FaHeart, FaHeartBroken } from "react-icons/fa";
 
+export default function Produto(){
+    const [favorito, setFavorito] = useState(false)
+
+    function switchFavorito(){
+        setFavorito(!favorito)
+    }
+    return(
+        <div className="container">
+            <div className="favorito" onClick={switchFavorito}>
+                {favorito ? <FaHeartBroken/> : <FaHeart/>}
+            </div>
+            <div className="preco">
+
+            </div>
+        </div>
+    )
+}

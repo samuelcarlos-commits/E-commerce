95de9b67f541183a1ba2dc8eb552f0bf138d36c8
samuelcarlos-commits <samuel.carlos@escolar.ifrn.edu.br>
2026-09-12 08:10:51 -0300

    modified:   meu-app/src/App.jsx 	modified:   meu-app/src/Produto.jsx 	modified:   meu-app/src/style.css

--- meu-app/src/App.jsx
+++ meu-app/src/App.jsx
@@ -5,11 +5,19 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const produtos = []
+
+  produtos.push({
+    nome: "macumba",
+    descricao: "amaldiçoe seus amigos!",
+    preco: "9.99",
+    imagem: "/assets/hero.png"
+  })
 
   return (
     <>
       <Navbar></Navbar>
-      <Produto></Produto>
+      <Produto produto={produtos[0]}></Produto>
     </>
   )
 }
--- meu-app/src/Produto.jsx
+++ meu-app/src/Produto.jsx
@@ -1,20 +1,30 @@
 import "./style.css"
-import { useState } from 'react'
-import { FaHeart, FaHeartBroken } from "react-icons/fa";
+import { useState } from "react"
+import { FaHeart, FaHeartBroken } from "react-icons/fa"
 
-export default function Produto(){
+export default function Produto({ produto }) {
     const [favorito, setFavorito] = useState(false)
+    const { nome, descricao, preco, imagem } = produto
 
-    function switchFavorito(){
+    function switchFavorito() {
         setFavorito(!favorito)
     }
-    return(
-        <div className="container">
-            <div className="favorito" onClick={switchFavorito}>
-                {favorito ? <FaHeartBroken/> : <FaHeart/>}
-            </div>
-            <div className="preco">
 
+    return (
+        <div className="container">
+            <div className="imagem">
+                <img src={imagem} alt={nome} />
+            </div>
+
+            <h3>{nome}</h3>
+            <p>{descricao}</p>
+
+            <div className="preco">
+                <p>{preco}</p>
+            </div>
+
+            <div className="favorito" onClick={switchFavorito}>
+                {favorito ? <FaHeartBroken /> : <FaHeart />}
             </div>
         </div>
     )
